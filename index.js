00e2445156fe3e40f1d26cb0b211d743928a4ceb
zeniagist <zeniagist@gmail.com>
2021-02-15 19:58:24 -0600

    added Morgan middleware library and express static

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-const express = require('express');
-
+// initialize express.js
+const express = require('express'),
+    morgan = require('morgan');
 const  app = express();
 
+// JSON movie list
+let movieList = [
+    {
+        title: 'Harry Potter and the Sorcerer\'s Stone',
+        author: 'J.K. Rowling'
+      },
+      {
+        title: 'Lord of the Rings',
+        author: 'J.R.R. Tolkien'
+      },
+      {
+        title: 'Twilight',
+        author: 'Stephanie Meyer'
+      }
+];
+
+// GET requests
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>');
+    res.send('<h1>Welcome to the Movie Database</h1>');
 } );
 
-const PORT = process.env.PORT || 5000;
+// express static
+app.use(express.static('public'));
 
+// Morgan middleware library
+app.use(morgan('common'));
+
+app.get('/movies', (req, res) => {
+    res.json(movieList);
+});
+
+app.get('/search', (req, res) => {
+    res.send('Search for movies here');
+});
+
+// listen for requests
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
