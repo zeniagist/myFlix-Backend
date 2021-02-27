aed62156e9875e273d2d3be01b47e139abfd7b9e
Zenia Gist <zeniagist@gmail.com>
2021-02-27 15:04:05 -0600

    mongoose schema and models configured to query myFlixDB database

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const express = require('express'),
     morgan = require('morgan');
 const  app = express();
 
+// require mongoose
+const mongoose = require('mongoose');
+const Models = require('./models.js');
+
+const Movies = Models.Movie;
+const Users = Models.User;
+
+mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+
 // JSON movie list
 let movieList = [
     {
