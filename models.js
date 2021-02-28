effe18637f3d51daedfcb7a8625a9f9d0b07e067
Zenia Gist <zeniagist@gmail.com>
2021-02-28 15:27:33 -0600

    initialized CORS and bcrypt

--- models.js
+++ models.js
@@ -1,4 +1,4 @@
-// initialize Mongoose package
+// require Mongoose package
 const mongoose = require('mongoose');
 
 let movieSchema = mongoose.Schema({
@@ -16,6 +16,9 @@ let movieSchema = mongoose.Schema({
     Featured: Boolean
 });
 
+// require bcrypt
+const bcrypt = require('bcrypt');
+
 let userSchema = mongoose.Schema({
     Username: {type: String, required: true},
     Password: {type: String, required: true},
@@ -23,6 +26,14 @@ let userSchema = mongoose.Schema({
     Birthday: Date,
     FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
+
+userSchema.statics.hashPassword = (password) => {
+    return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = function(password) {
+    return bcrypt.compareSync(password, this.Password);
+};
 
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
