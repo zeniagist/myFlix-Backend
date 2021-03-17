5dd99c0388b3b7d1bc28a3287f4514acdb399bcb
Zenia Gist <zeniagist@gmail.com>
2021-03-16 22:22:11 -0500

    added all port access

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 //Middleware functions
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'http://localhost:60216'];
+let allowedOrigins = ['*'];
 
 app.use(cors({
   origin: (origin, callback) => {
